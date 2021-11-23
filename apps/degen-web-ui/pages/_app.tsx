3476f7214968c29930e11c86581519d07064235b
Daniel-Carroll <danielcarroll95@gmail.com>
2021-11-23 14:20:22 -0600

    add next-auth configure discord provider

--- apps/degen-web-ui/pages/_app.tsx
+++ apps/degen-web-ui/pages/_app.tsx
@@ -3,6 +3,8 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
 import Layout from '../src/layout/layout';
 import './styles.scss';
+import React from 'react';
+import { Provider } from 'next-auth/client';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,9 +13,17 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Degen Bot</title>
       </Head>
       <ChakraProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Provider
+          options={{
+            clientMaxAge: 0,
+            keepAlive: 0,
+          }}
+          session={pageProps.session}
+        >
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
       </ChakraProvider>
     </>
   );
