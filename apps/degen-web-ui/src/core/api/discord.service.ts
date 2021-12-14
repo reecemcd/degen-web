e9fec974c248b2494c778c4b4975dda558929d3f
Daniel-Carroll <danielcarroll95@gmail.com>
2021-12-13 19:53:09 -0600

    feat: add get guild channels

--- apps/degen-web-ui/src/core/api/discord.service.ts
+++ apps/degen-web-ui/src/core/api/discord.service.ts
@@ -1,11 +1,18 @@
 import { IncomingMessage } from 'http';
 import { getSession } from 'next-auth/react';
 import { DiscordSession } from '../interfaces/auth-session';
-import { Client as DiscordClient, Guild, GuildMember, Permissions } from 'discord.js';
+import {
+  Client as DiscordClient,
+  Guild,
+  GuildChannel,
+  GuildMember,
+  Permissions,
+} from 'discord.js';
 import { REST as DiscordRest } from '@discordjs/rest';
 import { GuildDTO } from '../interfaces/guild.dto';
 import { ServerGlobals } from '../../server';
 import { PoapParticipantDTO } from '../interfaces/poap-participant';
+import { ChannelDTO } from '../interfaces/channel.dto';
 
 export class DiscordService {
   private rest: DiscordRest;
@@ -51,6 +58,17 @@ export class DiscordService {
     );
   }
 
+  async getGuildChannels(guildId: string): Promise<ChannelDTO[]> {
+    const guild = await this.client.guilds.fetch(guildId);
+    const channels = await guild.channels.fetch();
+
+    const voiceChannels = await Promise.all(
+      channels.map((channel) => this.transformChannelResponse(channel))
+    );
+
+    return voiceChannels.filter((channel) => channel.type === 'GUILD_VOICE');
+  }
+
   async getMembersInChannel(
     guildId: string,
     voiceChannelId: string
@@ -74,6 +92,19 @@ export class DiscordService {
         name: guild.name,
         iconUrl: guild.iconURL(),
         guildAdmin: await this.isGuildAdmin(guild.id, guildMember),
+      };
+    }
+    return null;
+  }
+
+  private async transformChannelResponse(
+    guildChannel: GuildChannel
+  ): Promise<ChannelDTO> {
+    if (guildChannel) {
+      return {
+        id: guildChannel.id,
+        name: guildChannel.name,
+        type: guildChannel.type,
       };
     }
     return null;
